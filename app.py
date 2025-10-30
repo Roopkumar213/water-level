29a424b4f0051eba4241407f2f3b4d49cff150d9
Roopkumar213 <roopkumar3244@gmail.com>
2025-10-30 22:32:53 +0530

    Initial commit

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 import pickle
 import numpy as np
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -38,5 +39,6 @@ def predict():
     except Exception as e:
         return jsonify({"error": str(e)})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
